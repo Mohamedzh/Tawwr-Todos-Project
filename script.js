edc0a59660d38a5e92186ea0c177f955b79816a7
Mohamedzh <50187724+Mohamedzh@users.noreply.github.com>
2022-06-12 18:32:33 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,62 @@
+const b1 = document.getElementById("b1");
+const list = document.getElementById("tasklist")
+let listarray;
+
+
+b1.addEventListener('click', todos);
+
+//retrive from local storage
+localstore();
+function localstore(){
+    if (localStorage.getItem("mylist")===null){
+listarray = [];}else{
+    listarray = JSON.parse(localStorage.getItem("mylist"));
+    let i=0;
+    while (i < listarray.length){
+
+        
+            list.innerHTML += `<div class="entry"><li class="entryli" class="list-group-item active">${listarray[i]}
+            <button type="button" id = "check"><i class="fa-solid fa-square-check"></i></button>
+            <button type = "button" id = "del"><i class="fa-solid fa-trash-can"></i></button>
+            </li></div>`;
+            i += 1;
+    };
+    console.log(listarray);
+}};
+//The todos function
+function todos(e) {
+    e.preventDefault();
+    var newtask = document.getElementById("task").value;
+    
+
+var newTa = `<div class="entry"><li class="entryli" class="list-group-item active">${newtask}
+<button type="button" id = "check"><i class="fa-solid fa-square-check"></i></button>
+<button type = "button" id = "del"><i class="fa-solid fa-trash-can"></i></button>
+</li></div>`;
+    list.innerHTML += newTa;
+    listarray.push(newtask);
+    localStorage.setItem('mylist', JSON.stringify(listarray));
+
+    
+    document.getElementById("container").reset();
+}
+
+//Buttons
+
+list.addEventListener('click', buttons);
+function buttons(e){
+    const item = e.target;
+    
+    if (item.id === 'check'){
+        //item.parentElement.innertex.strike();
+        item.parentElement.classList.toggle("strike");
+    };
+    if (item.id === 'del'){
+        item.parentElement.remove();
+
+    }
+}
+
+
+
+    
