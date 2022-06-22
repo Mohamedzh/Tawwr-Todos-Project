51f3f690669d6f70fe4bdf57fc894e16afe57398
Mohamedzh <50187724+Mohamedzh@users.noreply.github.com>
2022-06-22 21:55:32 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -15,45 +15,52 @@ listarray = [];}else{
     while (i < listarray.length){
 
         
-            list.innerHTML += `<div class="entry"><li class="entryli" class="list-group-item active">${listarray[i]}
+            list.innerHTML += `<li class="entryli" class="list-group-item active">${listarray[i]}
             <button type="button" id = "check"><i class="fa-solid fa-square-check"></i></button>
             <button type = "button" id = "del"><i class="fa-solid fa-trash-can"></i></button>
-            </li></div>`;
+            </li>`;
             i += 1;
     };
     console.log(listarray);
 }};
+
 //The todos function
 function todos(e) {
     e.preventDefault();
     var newtask = document.getElementById("task").value;
-    
+    if (newtask == "" || newtask == "Write your task here") {
+        alert("Empty entry");
+        document.getElementById("container").reset();
+    } else {
 
-var newTa = `<div class="entry"><li class="entryli" class="list-group-item active">${newtask}
+var newTa = `<li class="entryli" class="list-group-item active">${newtask}
 <button type="button" id = "check"><i class="fa-solid fa-square-check"></i></button>
 <button type = "button" id = "del"><i class="fa-solid fa-trash-can"></i></button>
-</li></div>`;
+</li>`;
     list.innerHTML += newTa;
     listarray.push(newtask);
     localStorage.setItem('mylist', JSON.stringify(listarray));
 
     
     document.getElementById("container").reset();
-}
+    console.log(listarray);
+}}
 
 //Buttons
 
 list.addEventListener('click', buttons);
 function buttons(e){
     const item = e.target;
-    
+
     if (item.id === 'check'){
-        //item.parentElement.innertex.strike();
         item.parentElement.classList.toggle("strike");
     };
     if (item.id === 'del'){
         item.parentElement.remove();
-
+        let i = listarray.indexOf(item.parentElement.innerText);
+        listarray.splice(i, 1);
+        
+        localStorage.setItem('mylist', JSON.stringify(listarray));
     }
 }
 
